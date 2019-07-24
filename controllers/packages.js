c86b6da7faec8394d0805ab6b8287a16a45c3599
reserviert97 <nurlatif97@outlook.com>
2019-07-24 13:08:23 +0700

    fix user

--- controllers/packages.js
+++ controllers/packages.js
@@ -5,82 +5,94 @@ const connection = require('../config/connect');
 const isEmpty = require('lodash.isempty');
 
 exports.showPackages = (req, res) => {
-    let search = req.query.search;
-    let query = "SELECT *FROM packages";
-    
-    if(!isEmpty(search)){
-        query += ` WHERE packages.package_name LIKE '%${req.query.search}%' OR packages.package_city LIKE '%${req.query.search}%'`;
+  let search = req.query.search;
+  let query = 'SELECT *FROM packages';
+
+  if (!isEmpty(search)) {
+    query += ` WHERE packages.package_name LIKE '%${
+      req.query.search
+    }%' OR packages.package_city LIKE '%${req.query.search}%'`;
+  }
+
+  connection.query(query, (error, rows, field) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows != '') {
+        res.status(200).json({
+          status: 200,
+          data: rows
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          data: 'Data not found !'
+        });
+      }
     }
+  });
+};
 
-    connection.query(query, (error, rows, field) => {
-        if (error) {
-            console.log(error)
-        } else {
-            if (rows != "") {
-                res.status(200).json({
-                    status: 200,
-                    data: rows
-                })
+exports.showPackagesById = (req, res) => {
+  let id = req.params.id;
+  let fetchDestination = `SELECT *FROM packages LEFT JOIN destinations ON destinations.id_package = packages.id WHERE destinations.id_package = ${id}`;
+  let query = `SELECT * FROM packages WHERE packages.id = ${id}`;
+  console.log(req.userData);
+
+  connection.query(fetchDestination, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (data != '') {
+        connection.query(query, (error, rows, field) => {
+          if (error) {
+            console.log(error);
+          } else {
+            if (rows != '') {
+              res.status(200).json({
+                status: 200,
+                data: { ...rows[0], destinations: data }
+              });
+            } else {
+              res.status(404).json({
+                status: 404,
+                data: 'Data not found !'
+              });
             }
-            else {
-                res.status(404).json({
-                    status: 404,
-                    data: 'Data not found !'
-                })
-            }
-        }
-    })
-}
-
-exports.showPackagesById = (req,res) => {
-    let id = req.params.id;  
-    let query = `SELECT *FROM packages LEFT JOIN destinations ON destinations.id_package = packages.id WHERE packages.id = ${id}`;
-
-    connection.query(query, (error, rows, field) => {
-        if (error) {
-            console.log(error)
-        } else {
-            if (rows != "") {
-                res.status(200).json({
-                    status: 200,
-                    data: rows
-                })
-            }
-            else {
-                res.status(404).json({
-                    status: 404,
-                    data: 'Data not found !'
-                })
-            }
-        }
-    })
-
-
-}
+          }
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          data: 'Data not found !'
+        });
+      }
+    }
+  });
+};
 
 // Destination
 
 exports.destinationById = (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const query = `SELECT *FROM packages LEFT JOIN destinations ON destinations.id_package = packages.id WHERE destinations.id_package = ${id}`;
-    connection.query(query, (error, rows, field) => {
-        if (error) {
-            console.log(error)
-        } else {
-            if (rows != "") {
-                res.status(200).json({
-                    status: 200,
-                    id_package: parseInt(id),
-                    data: rows
-                })
-            }
-            else {
-                res.status(404).json({
-                    status: 404,
-                    data: 'Data not found !'
-                })
-            }
-        }
-    })
-}
+  const query = `SELECT * FROM packages LEFT JOIN destinations ON destinations.id_package = packages.id WHERE destinations.id_package = ${id}`;
+  connection.query(query, (error, rows, field) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows != '') {
+        res.status(200).json({
+          status: 200,
+          id_package: parseInt(id),
+          data: rows
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          data: 'Data not found !'
+        });
+      }
+    }
+  });
+};
--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,14 +1,15 @@
-const key = process.env.STRIPE_TEST_KEY
-const stripe = require("stripe")(key);
+const key = process.env.STRIPE_TEST_KEY;
+const stripe = require('stripe')(key);
 
-exports.stripe = async function(req, res){
-    const number = req.body.number || '4242424242424242';
-    const month = req.body.month || 12;
-    const year = req.body.year || 2020;
-    const cvc = req.body.cvc || '123';
-    const amount = req.body.amount || 1000000;
-    try {
-      stripe.tokens.create({
+exports.stripe = async function(req, res) {
+  const number = req.body.number || '4242424242424242';
+  const month = req.body.month || 12;
+  const year = req.body.year || 2020;
+  const cvc = req.body.cvc || '123';
+  const amount = req.body.amount || 1000000;
+  try {
+    stripe.tokens
+      .create({
         card: {
           number: number,
           exp_month: month,
@@ -16,21 +17,20 @@ exports.stripe = async function(req, res){
           cvc: cvc
         }
       })
-        .then((token) => {
-          return stripe.charges.create({
-            amount: amount,
-            currency: 'idr',
-            source: token.id,
-          });
-        })
-        .then((result) => {
-          res.status(200).json({status: 200, message: "payment succesfull"})
-        })
-        .catch((err) => {
-          res.status(400).json(err)
+      .then(token => {
+        return stripe.charges.create({
+          amount: amount,
+          currency: 'idr',
+          source: token.id
         });
-    }
-    catch (err) {
-      res.status(500).json(err);
-    }
+      })
+      .then(result => {
+        res.status(200).json({ status: 200, message: 'payment succesfull' });
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  } catch (err) {
+    res.status(500).json(err);
   }
+};
